76ff35695d43753f8e015f75aba5618b6e799de7
Kausar Khan <156500913+KausarKhan-2002@users.noreply.github.com>
2024-11-15 17:50:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //your JS code here. If required.
+
+let codeContainer = document.querySelector(".code-container");
+let codeInput = document.querySelectorAll(".code");
+
+// Focus on the first input on page load
+window.addEventListener("load", () => {
+  codeContainer.firstElementChild.focus();
+});
+
+
+// To focus on next input element
+const focusNextInput = (e) => {
+  if (e.target.nextElementSibling && e.key != "Backspace") {
+    e.target.nextElementSibling.focus();
+  }
+};
+
+const focusPreviousInput = (e) => {
+    if (e.key == "Backspace") {
+        e.target.value = ""
+        if (e.target.previousElementSibling) {
+            e.target.previousElementSibling.focus();
+        }
+    }
+}
+
+codeInput.forEach((code) => {
+  code.addEventListener("keyup", (e) => {
+    e.target.value = e.target.value[0];
+
+    focusNextInput(e)
+    focusPreviousInput(e)
+    
+  })
+});
