79db63cb3ee9b3654b5e26f804eeb1189ccd32c9
Kausar Khan <156500913+KausarKhan-2002@users.noreply.github.com>
2024-12-08 22:54:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,23 @@
 //your JS code here. If required.
 
-let codeContainer = document.querySelector(".code-container");
-let codeInput = document.querySelectorAll(".code");
+const codes = document.querySelectorAll('.code');
 
-// Focus on the first input on page load
-// window.addEventListener("load", () => {
-//   codeContainer.firstElementChild.focus();
-// });
-
-// To focus on next input element
-const focusNextInput = (e) => {
-  if (e.target.nextElementSibling && e.key != "Backspace") {
-    e.target.nextElementSibling.focus();
-  }
-};
-
-const focusPreviousInput = (e) => {
-    if (e.key == "Backspace") {
-        e.target.value = ""
-        if (e.target.previousElementSibling) {
-            e.target.previousElementSibling.focus();
-        }
+codes.forEach((code, index) => {
+  code.addEventListener('input', (e) => {
+    if (e.target.value) {
+      const nextInput = codes[index + 1];
+      if (nextInput) {
+        nextInput.focus();
+      }
     }
-}
+  });
 
-codeInput.forEach((code) => {
-  code.addEventListener("keyup", (e) => {
-    e.target.value = e.target.value[0];
-
-    focusNextInput(e)
-    focusPreviousInput(e)
-    
-  })
+  code.addEventListener('keydown', (e) => {
+    if (e.key === 'Backspace' && !e.target.value) {
+      const previousInput = codes[index - 1];
+      if (previousInput) {
+        previousInput.focus();
+      }
+    }
+  });
 });
